3ca39be5500a9026683dd159f152561acca9084a
Kinga Mikołajczyk <kinga.mikola@gmail.com>
2023-09-09 07:54:45 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { nanoid } from 'nanoid';
 import ContactList from './ContactList/index';
 import ContactForm from './ContactForm/index';
